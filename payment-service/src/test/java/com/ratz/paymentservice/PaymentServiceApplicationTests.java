9a2e5d01ed40fd13aadb84806e9835580911410c
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-10-10 10:16:02 +0000

    added payment service

---
+++ payment-service/src/test/java/com/ratz/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratz.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
