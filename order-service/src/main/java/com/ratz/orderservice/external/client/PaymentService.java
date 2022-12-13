d7a69380179e0b9c31eb29e062a1fb6040cc8fcc
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-12-13 10:21:30 -0100

    implemented circuit breaker in order service

--- order-service/src/main/java/com/ratz/orderservice/external/client/PaymentService.java
+++ order-service/src/main/java/com/ratz/orderservice/external/client/PaymentService.java
@@ -1,14 +1,22 @@
 package com.ratz.orderservice.external.client;
 
+import com.ratz.orderservice.exception.CustomException;
 import com.ratz.orderservice.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/api/payment")
 public interface PaymentService {
 
     @PostMapping
     ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e){
+
+        throw new CustomException("Payment service is down","UNAVAILABLE", 500);
+    }
 }
--- order-service/src/main/java/com/ratz/orderservice/external/client/ProductService.java
+++ order-service/src/main/java/com/ratz/orderservice/external/client/ProductService.java
@@ -1,14 +1,22 @@
 package com.ratz.orderservice.external.client;
 
+import com.ratz.orderservice.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/api/product")
 public interface ProductService {
 
     @PutMapping("/reduceQuantity/{id}")
     ResponseEntity<Void> reduceQuantity(@PathVariable long id, @RequestParam long quantity);
+
+    default void fallback(Exception e){
+
+        throw new CustomException("Product service is down","UNAVAILABLE", 500);
+    }
 }
